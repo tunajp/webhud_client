7f3ca9377a1ae19b1cf56229dbccb5b503ce00b8
tunajp <tunajp@gmail.com>
2022-01-17 16:02:23 +0900

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,32 @@
 import React from 'react';
 import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
+    ChakraProvider,
+    Box,
+    Text,
+    Link,
+    VStack,
+    Code,
+    Grid,
+    theme,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
-import MyButton from './MyButton'
+export default class App extends React.Component {
+    render() {
+        return (
+            <ChakraProvider theme={theme}>
+                <Box textAlign="center" fontSize="xl">
+                    <Grid minH="100vh" p={3}>
+                        <ColorModeSwitcher justifySelf="flex-end" />
+                        <VStack spacing={8}>
+                            <Text>
+                                Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+                            </Text>
+                        </VStack>
+                    </Grid>
+                </Box>
+            </ChakraProvider>
+        );
 
-function App() {
-  return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-        <MyButton name="ほげ"></MyButton>
-      </Box>
-    </ChakraProvider>
-  );
+    }
 }
-
-export default App;
